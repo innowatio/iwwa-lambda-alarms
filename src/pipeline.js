aea1787d9b919e154927cc1e2300da17a56f7e57
Davide Bianchi <davide.bianchi@mondora.com>
2016-10-18 18:17:55 +0200

    Fixed rule in utils. Fixed logs.

--- src/pipeline.js
+++ src/pipeline.js
@@ -7,7 +7,7 @@ import skipReading from "./steps/skip-reading";
 import spreadReadingByMeasurementType from "./steps/spread-reading-by-measurement-type";
 
 export default async function pipeline (event) {
-    log.info("Received readings event", event);
+    log.info(event, "Received readings event");
     /*
     *   Workaround: some events have been incorrectly generated and thus don't
     *   have an `element` property. When processing said events, just return and
@@ -19,7 +19,7 @@ export default async function pipeline (event) {
     }
     // find all the alarms for the selected sensor
     const alarms = await findAlarmsBySensor(rawReading.sensorId);
-    log.info("Alarms for selected sensors", alarms);
+    log.info(alarms, "Alarms for selected sensors");
     const readings = spreadReadingByMeasurementType(rawReading);
     await map(alarms, alarm => {
         return map(readings, async reading => await checkAndUpdateAlarm(alarm, reading, event));
--- src/steps/check-and-update-alarm.js
+++ src/steps/check-and-update-alarm.js
@@ -10,17 +10,17 @@ import updateAlarmAggregate from "./update-alarm-aggregate";
 import upsertAggregate from "./upsert-aggregate";
 
 export default async function checkAndUpdateAlarm (alarm, reading, event) {
-    log.debug("alarm", alarm);
+    log.debug(alarm, "alarm");
     // Skip if alarm not match the rule
     if (!siftValidator(alarm.rule, reading)) {
-        log.info("ALARM SKIPPED BY RULE");
+        log.info(alarm, "ALARM SKIPPED BY RULE");
         return null;
     }
     const alarmReadingValue = await getAlarmReadingValue(alarm, reading);
     const alarmTriggerStatus = siftValidator(alarm.thresholdRule, alarmReadingValue); // boolean
-    log.debug("Alarm trigger status", alarmTriggerStatus);
+    log.debug(alarmTriggerStatus, "Alarm trigger status");
     if (alarm.type !== "realtime" && !alarmTriggerStatus) {
-        log.info("Periodic alarm skipped", alarm);
+        log.info(alarm, "Periodic alarm skipped");
         return null;
     }
     const alarmAggregate = await getOrCreateAlarmAggregate(reading, alarm);
