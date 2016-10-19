0619fc83e6fda3c6428bd0f9edcab3aa36599ff5
Davide Bianchi <davide.bianchi@mondora.com>
2016-10-19 14:55:25 +0200

    Removed unused config

--- src/config.js
+++ src/config.js
@@ -4,7 +4,6 @@ dotenv.load({silent: true});
 
 export const MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/test";
 export const LOG_LEVEL = process.env.LOG_LEVEL || "info";
-export const NOTIFICATIONS_API_ENDPOINT = process.env.NOTIFICATIONS_API_ENDPOINT || "http://myapi.com/notifications";
 export const KINESIS_STREAM_NAME = process.env.KINESIS_STREAM_NAME || "test";
 
 export const ALARMS_COLLECTION_NAME = "alarms";
