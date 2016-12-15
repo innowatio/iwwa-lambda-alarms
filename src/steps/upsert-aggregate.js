a4cb6fc5efbd51f979fa5a92dbc02edf767c9c4b
Roberto Dell'Oro <roberto.delloro@mondora.com>
2016-12-15 15:37:49 +0100

    fix on upset.

--- src/steps/upsert-aggregate.js
+++ src/steps/upsert-aggregate.js
@@ -5,7 +5,7 @@ export default async function upsertAggregate (aggregate) {
     const db = await getMongoClient();
     return db.collection(ALARMS_AGGREGATES_COLLECTION_NAME).update(
         {_id: aggregate._id},
-        {$set: aggregate},
+        aggregate,
         {upsert: true}
     );
 }
